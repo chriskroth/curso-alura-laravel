11879a65af5b54d3ca10902117c725bb10e75be6
Chris <christian@liquidworks.com.br>
2022-07-31 19:46:08 -0300

    Incluído mensagens flash de sucesso.

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -8,8 +8,9 @@ use Illuminate\Http\Request;
 class SeriesController extends Controller {
     public function index() {
         $series = Serie::query()->orderBy("nome")->get();
+        $mensagemSucesso = session("mensagem.sucesso");
 
-        return view("series.index", compact('series'));
+        return view("series.index", compact('series', 'mensagemSucesso'));
     }
 
     public function create() {
@@ -19,16 +20,19 @@ class SeriesController extends Controller {
     public function store(Request $request) {
 
         Serie::create($request->all());
-//        Serie::create($request->only(['nome']));
-//        Serie::create($request->except(['_token']));
+        //Serie::create($request->only(['nome']));
+        //Serie::create($request->except(['_token']));
 
-//        return redirect("/series");
+        //return redirect("/series");
+
+        session()->flash("mensagem.sucesso", "Série incluída com sucesso!");
         return redirect()->route("series.index");
     }
 
     public function destroy(Request $request) {
         Serie::destroy($request->id);
 
+        session()->flash("mensagem.sucesso", "Série removida com sucesso!");
         return redirect()->route("series.index");
     }
 }
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,6 +1,12 @@
 <x-layout titulo="Séries">
     <a href="{{ route("series.create") }}" class="btn btn-dark mb-2">Nova série</a>
 
+    @isset($mensagemSucesso)
+        <div class="alert alert-success">
+            {{ $mensagemSucesso }}
+        </div>
+    @endisset
+
     <ul class="list-group">
         @foreach($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">

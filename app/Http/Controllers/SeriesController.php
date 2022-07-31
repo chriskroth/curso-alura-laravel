7f83b57a0f9dac84007b969703f411faf8ed2222
Chris <christian@liquidworks.com.br>
2022-07-31 17:47:26 -0300

    Criado migration de series; Criado Model Serie; Atualização da Controller para utilização da Model; Finalização do primeiro curso de Laravel.

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,21 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller {
     public function index() {
-        $series = [
-            "Stranger Things",
-            "The Boys",
-            "Loki",
-            "Gray's Anathomy"
-        ];
+        $series = Serie::query()->orderBy("nome")->get();
 
         return view("series.index", compact('series'));
     }
 
     public function create() {
         return view('series.create');
+    }
+
+    public function store(Request $request) {
+        $nome = $request->input("nome");
+
+        $serie = new Serie();
+        $serie->nome = $nome;
+        $serie->save();
+
+        return redirect("/series");
     }
 }
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,9 +1,9 @@
 <x-layout titulo="Séries">
-    <a href="series/criar" class="btn btn-dark mb-2">Nova série</a>
+    <a href="/series/criar" class="btn btn-dark mb-2">Nova série</a>
 
     <ul class="list-group">
         @foreach($series as $serie)
-            <li class="list-group-item">{{ $serie }}</li>
+            <li class="list-group-item">{{ $serie->nome }}</li>
         @endforeach
     </ul>
 </x-layout>
